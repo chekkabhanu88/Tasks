d02dd33ec765700f67821040be4410724be1609b
chekkabhanu88 <138876647+chekkabhanu88@users.noreply.github.com>
2023-07-07 16:41:58 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+package loan;
+
+public class loanapp {
+        double amount;
+        double emi;
+	    double roi;
+	    double tenure;
+	    
+	   public loanapp(double amount,double emi,double roi,double tenure)
+	   {
+		   this.amount=amount;
+		   this.emi=emi;
+		   this.roi=roi;
+		   this.tenure=tenure;
+	   }
