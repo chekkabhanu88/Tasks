3461612b3c583e96249049bc4bb143532672772d
chekkabhanu88 <138876647+chekkabhanu88@users.noreply.github.com>
2023-07-24 08:55:19 +0530

    Update Address.java

--- assignment/Address.java
+++ assignment/Address.java
@@ -1,5 +1,36 @@
 package assignment;
 
-public class Address {
+public class Address 
+{
+	private int ano;
+	private int apincode;
+	private String adistrict;
+	public Address(int ano, int apincode, String adistrict) {
+		
+		this.ano = ano;
+		this.apincode = apincode;
+		this.adistrict = adistrict;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int getApincode() {
+		return apincode;
+	}
+	public void setApincode(int apincode) {
+		this.apincode = apincode;
+	}
+	public String getAdistrict() {
+		return adistrict;
+	}
+	public void setAdistrict(String adistrict) {
+		this.adistrict = adistrict;
+	}
+	
+	
+	
 
 }
