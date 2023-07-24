e4109d71875dbedfe85941f67334ba3689ebed14
chekkabhanu88 <138876647+chekkabhanu88@users.noreply.github.com>
2023-07-24 08:54:37 +0530

    Update traveller.java

--- assignment/traveller.java
+++ assignment/traveller.java
@@ -1,5 +1,52 @@
 package assignment;
 
-public class traveller {
+import java.time.LocalDate;
+
+public class traveller 
+{
+	private String name;
+	private int mobileno;
+	private String email;
+	private String flightid;
+	private LocalDate date;
+	public traveller(String name, int mobileno, String email, String flightid, LocalDate date) {
+		
+		this.name = name;
+		this.mobileno = mobileno;
+		this.email = email;
+		this.flightid = flightid;
+		this.date = date;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(int mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFlightid() {
+		return flightid;
+	}
+	public void setFlightid(String flightid) {
+		this.flightid = flightid;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
 
 }
