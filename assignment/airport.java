77812a9940134dce191fc7f42c1ce8f2e74f2a99
chekkabhanu88 <138876647+chekkabhanu88@users.noreply.github.com>
2023-07-24 08:51:51 +0530

    Update airport.java

--- assignment/airport.java
+++ assignment/airport.java
@@ -1,5 +1,62 @@
 package assignment;
 
-public class airport {
+import java.util.ArrayList;
 
+public class airport 
+{
+    Address address;
+    flight[] f;
+    String[] a;
+    
+    int index=0;
+    
+    ArrayList<flight> fl= new ArrayList<flight>();
+    
+    ArrayList<traveller> tl=new ArrayList<traveller>();
+    
+    
+    public boolean addflight(flight f2)
+    {
+    	  f[index++]=f2; 
+    	 boolean flag=true;
+    	  if(flag==true)
+    		  return true;
+    	  else
+    		  return false;
+    }
+    public  void  searchflight(String source,String destination)
+    {
+    	for(flight e:f)
+    	{
+    		if  ( e.getSource().equals(source) && e.getDestination().equals(destination))
+    		{
+    		     fl.add(e);		
+    		}
+    		
+    	}
+ 
+    }
+    public void bookticket(String flightid,traveller t)
+    {
+       for(flight e:f)
+       {
+    	   if(e.getFlightid()==flightid)
+    	   {
+    		   if(e.getAvailableseats()>0)
+    		   {
+    			   tl.add(t);
+    			   
+    		   }
+    	   }
+    	   e.setAvailableseats(e.getAvailableseats()-1);
+       }
+       
+    }
+    
+    
+    
+    
+    
+    
+	
 }
